b6a56cbae3fd8fd1d81901c94f381ae53942be5d
andyslucky <apstrick@uncg.edu>
2018-12-10 17:46:36 -0500

    Major changes to client struct

--- socklib.h
+++ socklib.h
@@ -28,16 +28,20 @@ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+Contributors: Andrew Strickland, JK2Designs
 */
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#define ssl_on 1
+#include <netdb.h>
 #ifdef ssl_on
 //ssl includs
 #include <openssl/evp.h>
@@ -55,11 +59,13 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // Abstract handle for a client socket. This is returned by client_socket where all the appropriate function
 // pointers are set. If the socket uses ssl, the io functions are set to the appropriate ssl functions.
 struct client_sock{
-	int fd,ssl;
+	int fd,ssl,prot,type;
+    const char* err;
 	ssize_t (*receive)(int sock_fd, char* buff, size_t count);
 	ssize_t (*sendall)(int sock_fd, const char* data, size_t length);
 	ssize_t (*send_msg)(int sock_fd,const char* data,size_t length);
-	ssize_t (*recv_msg)(int sock_fd, char* buff, size_t count);
+	const char* (*recv_msg)(int sock_fd);
+    int (*connect)(struct client_sock* cli, const char* hostname, int port);
 	void (*settimeout)(int sock_fd,int seconds);
 	int (*close)(int fd);
 };
@@ -80,6 +86,10 @@ struct server_sock{
 void init_ssl(int sock_fd,const char* path_to_chain, const char* path_to_key,int client);
 #endif
 
+int* read_select(int*sock_fds, unsigned long length, int timeout);
+
 struct server_sock server_socket(int prot,int type,int ssl);
 
 struct client_sock client_socket(int prot,int type, int ssl);
+
+
